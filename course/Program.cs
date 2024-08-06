ba3e12aae5e7818781e5dda53b53b7fa18d6f1f2
EnzoLsena <enzolevysena@gmail.com>
2024-08-05 23:05:02 -0300

    Add project files.

---
+++ course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            double a = 2.0, b = 5.0, c = 8;
+
+            double delta = Math.Pow(b,2.0) - 4.0 * a * c;
+
+            double x1 = (- b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            
+            Console.WriteLine(x1);
+            Console.WriteLine(delta);
+
+
+
+        }
+            
+    }
+}
